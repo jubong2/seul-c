700820211a185fe81c93ec65cb3fe23fce690fdd
jubong <twoweekyear2@gmail.com>
2024-10-30 17:05:39 +0900

    로그인 회원가입

--- js/swiper.js
+++ js/swiper.js
@@ -1,87 +1,25 @@
 window.addEventListener("load", function () {
-  var besttSwiper = new Swiper(".veganSwiper", {
-    pagination: {
-      el: ".swiper-pagination",
-      dynamicBullets: true,
-    },
-    slidesPerView: 1.5,
-    spaceBetween: 10,
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-    },
-    breakpoints: {
-      1200: { slidesPerView: 2.5 },
-      800: { slidesPerView: 2 },
-      600: { slidesPerView: 2, spaceBetween: 20 },
-    },
-    on: {
-      loop: true,
-    },
-  });
-  var besttSwiper = new Swiper(".dessertSwiper", {
-    pagination: {
-      el: ".swiper-pagination",
-      dynamicBullets: true,
-    },
-    slidesPerView: 1.5,
-    spaceBetween: 10,
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-    },
-    breakpoints: {
-      1200: { slidesPerView: 2.5 },
-      800: { slidesPerView: 2 },
-      600: { slidesPerView: 2, spaceBetween: 20 },
-    },
-    on: {
-      loop: true,
-    },
-  });
-  var besttSwiper = new Swiper(".mealSwiper", {
-    pagination: {
-      el: ".swiper-pagination",
-      dynamicBullets: true,
-    },
-    slidesPerView: 1.5,
-    spaceBetween: 10,
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-    },
-    breakpoints: {
-      1200: { slidesPerView: 2.5 },
-      800: { slidesPerView: 2 },
-      600: { slidesPerView: 2, spaceBetween: 20 },
-    },
-    on: {
-      loop: true,
-    },
-  });
-
-  var besttSwiper = new Swiper(".mealSwiper", {
-    pagination: {
-      el: ".swiper-pagination",
-      dynamicBullets: true,
-    },
-    slidesPerView: 1.5,
-    spaceBetween: 10,
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-    },
-    breakpoints: {
-      1200: { slidesPerView: 2.5 },
-      800: { slidesPerView: 2 },
-      600: { slidesPerView: 2, spaceBetween: 20 },
-    },
-    on: {
-      loop: true,
-    },
-  });
-
   // 티겟 스와이퍼
+  var besttSwiper = new Swiper(".bestSwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+    },
+    slidesPerView: 1.5,
+    spaceBetween: 10,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      1200: { slidesPerView: 3.5 },
+      800: { slidesPerView: 2 },
+      600: { slidesPerView: 2, spaceBetween: 20 },
+    },
+    on: {
+      loop: true,
+    },
+  });
   var targetSwiper = new Swiper(".targetSwiper", {
     pagination: {
       el: ".swiper-pagination",
